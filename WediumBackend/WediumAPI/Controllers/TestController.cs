52dc6fa868c822d44cb5a22a0df72bcdea912697
JainalGandhi <42400112+JainalGandhi@users.noreply.github.com>
2020-03-18 12:54:49 +1300

    Created ASP.NET initial skeleton

---
+++ WediumBackend/WediumAPI/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WediumAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new
+            {
+                Date = "today",
+                TemperatureC = 5,
+                Summary = true
+            });
+        }
+    }
+}
