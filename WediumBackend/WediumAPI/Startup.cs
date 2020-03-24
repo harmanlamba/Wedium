d4da61d3b680542548eae5214008a380458dda71
salma-s <ssan631@aucklanduni.ac.nz>
2020-03-24 22:31:44 +1300

    formatted

--- WediumBackend/WediumAPI/Startup.cs
+++ WediumBackend/WediumAPI/Startup.cs
@@ -27,7 +27,7 @@ namespace WediumAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-                services.AddDbContext<WediumContext>(options =>
+            services.AddDbContext<WediumContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("WediumDatabase")));
           
             services.AddControllers();
