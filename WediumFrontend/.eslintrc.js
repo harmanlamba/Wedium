2e54f092a6547ebc1ec7a3df8560a1a46b8db8fb
itsercleung <leung.eric24@gmail.com>
2020-03-28 17:39:13 +1300

    Changed eslintrc.js to support jest and jsx extension

--- WediumFrontend/.eslintrc.js
+++ WediumFrontend/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true
   },
   extends: [
     'plugin:react/recommended',
@@ -22,5 +23,8 @@ module.exports = {
     'react',
   ],
   rules: {
+    "react/jsx-filename-extension": [1, {
+      "extensions": [".js", ".jsx"]
+    }],
   },
 };
