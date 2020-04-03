af70c56165223098d1b4433f7ded816738921ce8
JainalGandhi <42400112+JainalGandhi@users.noreply.github.com>
2020-04-03 20:24:06 +1300

    Created integration tests for GetPostTypes

---
+++ WediumBackend/WediumTestSuite/PostTypeTest.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WediumTestSuite.Helper;
+
+namespace WediumTestSuite
+{
+    class PostTypeTest
+    {
+        private APIWebApplicationFactory _factory;
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _factory = new APIWebApplicationFactory();
+            _client = _factory.CreateClient();
+        }
+
+        [Test]
+        public async Task GetPostTypeTest()
+        {
+            HttpResponseMessage response = await _client.GetAsync("https://localhost:44300/api/PostType/Get");
+            JArray content = JArray.Parse(await response.Content.ReadAsStringAsync());
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual(13, content.Count);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+    }
+}
