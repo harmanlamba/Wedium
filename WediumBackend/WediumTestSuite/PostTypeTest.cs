b51c6b49759abe41d3435af9cb639d2606064be6
JainalGandhi <42400112+JainalGandhi@users.noreply.github.com>
2020-04-04 01:18:22 +1300

    Refactored TestServer initialisation into separate class

--- WediumBackend/WediumTestSuite/PostTypeTest.cs
+++ WediumBackend/WediumTestSuite/PostTypeTest.cs
@@ -6,20 +6,18 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WediumAPI;
+using WediumTestSuite.Helper;
 
 namespace WediumTestSuite
 {
     class PostTypeTest
     {
-        private TestServer _server;
         private HttpClient _client;
 
         [OneTimeSetUp]
         public void Setup()
         {
-            _server = new TestServer(new WebHostBuilder()
-                .UseStartup<Startup>());
-            _client = _server.CreateClient();
+            _client = TestServerInitialiser.GetClient();
         }
 
         [Test]
