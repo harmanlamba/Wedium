1a78221c5d788360ade9930d3bbdff2d5ca71194
JainalGandhi <42400112+JainalGandhi@users.noreply.github.com>
2020-04-04 21:09:11 +1300

    Renamed file to comply with casing standards

---
+++ WediumBackend/WediumAPI/Dto/PostTypeDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WediumAPI.Dto
+{
+    public class PostTypeDto
+    {
+        public string PostType { get; set; }
+    }
+}
