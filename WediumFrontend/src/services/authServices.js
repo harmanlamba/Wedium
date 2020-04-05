076c8b712a4811ae2d95c228c9684c0d1ec61dd6
itsercleung <leung.eric24@gmail.com>
2020-04-05 21:42:08 +1200

    Added const change for time

--- WediumFrontend/src/services/authServices.js
+++ WediumFrontend/src/services/authServices.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export function UserIsValid(token) {
     if (token.isAuthenticated) {
         const decodedToken = jwt.decode(token.user);
-        var dateNow = new Date()
+        const dateNow = new Date()
 
         if (decodedToken.exp > dateNow.getTime() / 1000) {
             return true;
