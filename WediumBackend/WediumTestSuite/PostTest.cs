39d4cd9ba46447e56b25dc5913000caf8d308e91
Harman Lamba <harrygth15478@gmail.com>
2020-05-05 00:53:11 +1200

    Added respective tests for post detail endpoint

--- WediumBackend/WediumTestSuite/PostTest.cs
+++ WediumBackend/WediumTestSuite/PostTest.cs
@@ -24,6 +24,10 @@ namespace WediumTestSuite
         private string _apiEndpoint;
 
         private readonly int USER_ID = 139;
+        private readonly int POST_ID = 70;
+        private readonly int WIKI_ARTICLE_ID = 73;
+        private readonly string ARTICLE_IMAGE_URL_COMMAND = "SELECT ArticleImageUrl, ArticleBody FROM WDM.[WikiArticle] where WikiArticleId = ";
+
 
         [OneTimeSetUp]
         public void Setup()
@@ -98,25 +102,84 @@ namespace WediumTestSuite
             Assert.AreEqual(limit, secondBatchContent.Count);
             Assert.AreEqual(firstBatchContent[limit - 1].PostId, secondBatchContent[0].PostId);
         }
+        
+        [Test]
+        public async Task GetSingularPostWithID()
+        {
+            HttpClient client = _testServer.CreateClient(USER_ID);
+            Task<(HttpStatusCode, PostDto)> response = CreatePost(client);
+
+            Assert.AreEqual(HttpStatusCode.Created, response.Result.Item1);
+
+            Post post = _db.Post.First(p => p.Title.Equals(response.Result.Item2.Title) && p.UserId == USER_ID);
+
+            HttpResponseMessage httpResponse = await client.GetAsync(_apiEndpoint + $"api/Post/get/{post.PostId}");
+            Assert.AreEqual(HttpStatusCode.OK, httpResponse.StatusCode);
+
+            PostDto postDto = await httpResponse.Content.ReadAsAsync<PostDto>();
+            Assert.AreEqual(response.Result.Item2.Title, postDto.Title);
+            Assert.AreEqual(response.Result.Item2.ArticleTitle, postDto.ArticleTitle);
+            Assert.AreEqual(response.Result.Item2.PostType, postDto.PostType);
+            Assert.AreEqual(response.Result.Item2.ArticleUrl, postDto.ArticleUrl);
+
+            //Deleting the Recently Created Post
+            HttpStatusCode statuscode = await DeletePostHelper(response.Result.Item2, USER_ID);
+            Assert.AreEqual(HttpStatusCode.OK, statuscode);
+
+        }
+
+        [Test]
+        public async Task GetSingularPostWithIDCheckWikiArticleData()
+        {
+            HttpClient client = _testServer.CreateClient();
+            HttpResponseMessage response = await client.GetAsync(_apiEndpoint + $"api/Post/get/{POST_ID}");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            PostDto postDto = await response.Content.ReadAsAsync<PostDto>();
+
+            string ArticleImageUrl = "";
+            string ArticleBody = "";
+
+            using (SqlConnection connection = new SqlConnection(DatabaseContextResolver.GetConnectionString()))
+            {
+                SqlCommand command = new SqlCommand(ARTICLE_IMAGE_URL_COMMAND + WIKI_ARTICLE_ID, connection);
+                connection.Open();
+                SqlDataReader dr = command.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    ArticleImageUrl = (string)dr["ArticleImageUrl"];
+                    ArticleBody = (string)dr["ArticleBody"];
+                }
+         
+            }
+
+            Assert.AreEqual(ArticleImageUrl, postDto.ArticleImageUrl);
+            Assert.AreEqual(ArticleBody, postDto.ArticleBody);
+          
+        }
+
+        [Test]
+        public async Task GetPostWithInvalidPostId()
+        {
+            HttpClient client = _testServer.CreateClient();
+            HttpResponseMessage response = await client.GetAsync(_apiEndpoint + $"api/Post/get/-1");
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+
+        }
 
         [Test]
         public async Task CreatePost_SuccessfulCreate()
         {
             HttpClient client = _testServer.CreateClient(USER_ID);
-
-            PostDto postDto = new PostDto
-            {
-                ArticleTitle = "Bugatti",
-                Title = "Bugatti or Shucatti",
-                PostType = "Culture",
-                ArticleUrl = "https://en.wikipedia.org/wiki/Bugatti"
-            };
-
-            HttpResponseMessage response = await client.PostAsync(_apiEndpoint + "api/Post/Post", postDto, new JsonMediaTypeFormatter());
-            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            Task<(HttpStatusCode,PostDto)> response = CreatePost(client);
+            
+            Assert.AreEqual(HttpStatusCode.Created, response.Result.Item1);
 
             //Deleting the Recently Created Post
-            HttpStatusCode statuscode = await DeletePostHelper(postDto, USER_ID);
+            HttpStatusCode statuscode = await DeletePostHelper(response.Result.Item2, USER_ID);
             Assert.AreEqual(HttpStatusCode.OK, statuscode);
         }
 
@@ -245,6 +308,21 @@ namespace WediumTestSuite
                 connection.Open();
                 command.BeginExecuteNonQuery();
             }
+        }
+
+        private async Task<(HttpStatusCode,PostDto)> CreatePost(HttpClient client)
+        {
+            PostDto postDto = new PostDto
+            {
+                ArticleTitle = "Bugatti",
+                Title = "Bugatti or Shucatti",
+                PostType = "Culture",
+                ArticleUrl = "https://en.wikipedia.org/wiki/Bugatti"
+            };
+
+            HttpResponseMessage response = await client.PostAsync(_apiEndpoint + "api/Post/Post", postDto, new JsonMediaTypeFormatter());
+
+            return (response.StatusCode,postDto);
         }
     }
 }
