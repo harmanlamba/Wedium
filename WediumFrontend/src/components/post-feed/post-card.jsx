f75462bee0cb79a2066dfc0ac74587121e5fbc23
JainalGandhi <42400112+JainalGandhi@users.noreply.github.com>
2020-05-05 23:31:52 +1200

    Removed border radius from cards

--- WediumFrontend/src/components/post-feed/post-card.jsx
+++ WediumFrontend/src/components/post-feed/post-card.jsx
@@ -54,13 +54,14 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         justifyContent: 'space-between',
         marginBottom: 24,
+        borderRadius: 0,
     },
     image: {
         width: 140,
         height: 128,
         border: '2px solid gainsboro',
         marginTop: 16,
-        marginRight: 8
+        marginRight: 16
     },
     articleLink: {
         marginLeft: 8,
