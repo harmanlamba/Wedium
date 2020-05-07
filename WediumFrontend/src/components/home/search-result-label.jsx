5924391d0e254592f8656294b7081d2055858244
salma-s <ssan631@aucklanduni.ac.nz>
2020-05-07 16:48:45 +1200

    Made search text and posrt type more prominent in the results label

--- WediumFrontend/src/components/home/search-result-label.jsx
+++ WediumFrontend/src/components/home/search-result-label.jsx
@@ -17,41 +17,48 @@ const SearchResultLabel = (props) => {
   const history = useHistory();
 
   return (
-    <Card className={classes.search}>
+    <Card className={classes.root}>
       <Typography variant="body1">Showing results for</Typography>
+
       <Typography variant="body1"
         color="primary"
         display="inline"
         className={classes.searchString}>
         {props.searchString}
       </Typography>
-      { props.postType && <div>
-        <Typography variant="body1" display="inline">in</Typography>
-        <Typography variant="body1"
-        color="primary"
-        display="inline"
-        className={classes.postType}
-        onClick={() => history.push(`/post/${props.postType}`)}>
-        {props.postType}
-      </Typography></div>}
-    
-    </Card>
 
+      {props.postType &&
+        <div>
+          <Typography variant="body1" display="inline">in</Typography>
+          <Typography variant="body1"
+            color="primary"
+            display="inline"
+            className={classes.postType}
+            onClick={() => history.push(`/post/${props.postType}`)}>
+            {props.postType}
+          </Typography>
+        </div>
+      }
+    </Card>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  search: {
+  root: {
     borderRadius: 0,
     padding: 10,
     display: 'flex',
+    fontWeight: 200
+
   },
   searchString: {
     margin: '0 4px',
+    fontWeight: 500
   },
   postType: {
     margin: '0 4px',
     cursor: 'pointer',
+    fontWeight: 500
   },
 }));
 
