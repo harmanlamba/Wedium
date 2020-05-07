f363e4e5ae4d29511426c8536fa3c8e6c380d5dc
JainalGandhi <42400112+JainalGandhi@users.noreply.github.com>
2020-05-07 14:02:48 +1200

    Removed usestate useeffect warnings

--- WediumFrontend/src/components/post-favourite/index.jsx
+++ WediumFrontend/src/components/post-favourite/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { tryFavouritePost, tryUnfavouritePost } from '../../redux/actions/thunk/post-favourite-thunk';
--- WediumFrontend/src/components/post-like/index.jsx
+++ WediumFrontend/src/components/post-like/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { tryLikePost, tryUnlikePost } from '../../redux/actions/thunk/post-like-thunk';
