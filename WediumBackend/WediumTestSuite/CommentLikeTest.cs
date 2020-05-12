81e01f5ccee548bf2f0fd13858392a8c21163d3a
JainalGandhi <42400112+JainalGandhi@users.noreply.github.com>
2020-05-13 10:52:31 +1200

    Created tests for CommentLike endpoints

---
+++ WediumBackend/WediumTestSuite/CommentLikeTest.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using WediumAPI.Dto;
+using WediumTestSuite.Helper;
+
+namespace WediumTestSuite
+{
+    class CommentLikeTest
+    {
+        private TestServerHandler _testServer;
+        private string _apiEndpoint;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _testServer = new TestServerHandler();
+
+            _apiEndpoint = AppSettingsResolver.GetSetting<string>("APIEndpointURI");
+        }
+
+        [Test]
+        public async Task LikingAndUnlikingCommentTest()
+        {
+            HttpClient client = _testServer.CreateClient(136);
+
+            CommentLikeDto commentLikeDto = new CommentLikeDto
+            {
+                CommentId = 5
+            };
+
+            HttpResponseMessage createResponse = await client.PostAsync(_apiEndpoint + "api/commentLike/post", commentLikeDto, new JsonMediaTypeFormatter());
+            Assert.AreEqual(HttpStatusCode.Created, createResponse.StatusCode);
+
+            HttpResponseMessage duplicateCreateResponse = await client.PostAsync(_apiEndpoint + "api/commentLike/post", commentLikeDto, new JsonMediaTypeFormatter());
+            Assert.AreEqual(HttpStatusCode.OK, duplicateCreateResponse.StatusCode);
+
+            HttpResponseMessage deleteResponse = await client.PostAsync(_apiEndpoint + "api/commentLike/delete", commentLikeDto, new JsonMediaTypeFormatter());
+            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.StatusCode);
+
+            HttpResponseMessage duplicateDeleteResponse = await client.PostAsync(_apiEndpoint + "api/commentLike/delete", commentLikeDto, new JsonMediaTypeFormatter());
+            Assert.AreEqual(HttpStatusCode.NoContent, duplicateDeleteResponse.StatusCode);
+        }
+
+        [Test]
+        public async Task LikeInvalidCommentIdTest()
+        {
+            HttpClient client = _testServer.CreateClient(136);
+
+            CommentLikeDto commentLikeDto = new CommentLikeDto
+            {
+                CommentId = -1
+            };
+
+            HttpResponseMessage createResponse = await client.PostAsync(_apiEndpoint + "api/postLike/post", commentLikeDto, new JsonMediaTypeFormatter());
+            Assert.AreEqual(HttpStatusCode.NotFound, createResponse.StatusCode);
+        }
+
+        [Test]
+        public async Task UnlikeInvalidCommentIdTest()
+        {
+            HttpClient client = _testServer.CreateClient(136);
+
+            CommentLikeDto commentLikeDto = new CommentLikeDto
+            {
+                CommentId = -1
+            };
+
+            HttpResponseMessage duplicateDeleteResponse = await client.PostAsync(_apiEndpoint + "api/postLike/delete", commentLikeDto, new JsonMediaTypeFormatter());
+            Assert.AreEqual(HttpStatusCode.NotFound, duplicateDeleteResponse.StatusCode);
+        }
+    }
+}
