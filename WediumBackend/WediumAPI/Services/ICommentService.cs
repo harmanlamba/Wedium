704c4534df56ce148f8653ae0a48dd29f8c20359
Harman Lamba <harrygth15478@gmail.com>
2020-05-13 20:38:02 +1200

    Added implementation Comment Endpoint

---
+++ WediumBackend/WediumAPI/Services/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WediumAPI.Dto;
+
+namespace WediumAPI.Services
+{
+    public interface ICommentService
+    {
+        public IEnumerable<CommentDto> GetCommentsForPost(int postId);
+        public Tuple<int,PostDto> CreateComment(CommentDto commentDto, int? userId);
+    }
+}
